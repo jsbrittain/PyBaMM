460bc656351e0fb73e9f860e85cd633bed4393aa
John Brittain <john.brittain@dtc.ox.ac.uk>
2023-05-18 12:16:21 +0100

    (wip) add dockerised installer for testing

---
+++ issue-files/docker/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+podman build --build-arg FORCE_FRESH_COPY=$(date +%Y%m%d-%H%M%S) --rm -f Dockerfile -t ubuntu:pybamm_mpi .
+podman run --rm -it -e "TERM=xterm-256color" --name pybamm_mpi ubuntu:pybamm_mpi
