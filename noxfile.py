0a585620ef5467c3ba5c5028d49142aa9dba71b7
John Brittain <jsb1982@gmail.com>
2024-10-17 20:05:05 +0100

    Relax Python version constraints on IREE-compiler

--- noxfile.py
+++ noxfile.py
@@ -40,11 +40,11 @@ def set_iree_state():
             # iree-compiler is currently only available as a wheel on macOS 13 (or
             # higher) and Python version 3.11
             mac_ver = int(platform.mac_ver()[0].split(".")[0])
-            if (not sys.version_info[:2] == (3, 11)) or mac_ver < 14:
+            if mac_ver < 13:
                 warnings.warn(
                     (
-                        "IREE is only supported on MacOS 13 (or higher) and Python"
-                        "version 3.11. Setting PYBAMM_IDAKLU_EXPR_IREE=OFF."
+                        "IREE is only supported on MacOS 13 (or higher). "
+                        "Setting PYBAMM_IDAKLU_EXPR_IREE=OFF."
                     ),
                     stacklevel=2,
                 )
